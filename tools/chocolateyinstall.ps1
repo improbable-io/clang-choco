9dcad663211a27a40d20b2f065d9e61c99083fa2
Peter Mounce <petermounce@improbable.io>
2017-03-06 12:19:47 +0000

    double quotes to allow interpolation

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,25 +1,25 @@
-﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+﻿$ErrorActionPreference = "Stop"; # stop on all errors
 
-$packageName= 'clang'
+$packageName= "clang"
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://releases.llvm.org/3.8.0/LLVM-3.8.0-win32.exe'
-$url64      = 'http://releases.llvm.org/3.8.0/LLVM-3.8.0-win64.exe'
+$url        = "http://releases.llvm.org/3.8.0/LLVM-3.8.0-win32.exe"
+$url64      = "http://releases.llvm.org/3.8.0/LLVM-3.8.0-win64.exe"
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
-  fileType      = 'EXE'
+  fileType      = "EXE"
   url           = $url
   url64bit      = $url64
-  softwareName  = 'LLVM'
-  checksum      = 'EBDB056EBC6EFD57C0643733A099920D1D5760A4D570580243E6C63F4B52920F'
-  checksumType  = 'sha256'
-  checksum64    = 'F9C3147157DB32BEAB025314DB9B82C600E182D82994880C1D844C70E29D76EF'
-  checksumType64= 'sha256'
+  softwareName  = "LLVM"
+  checksum      = "EBDB056EBC6EFD57C0643733A099920D1D5760A4D570580243E6C63F4B52920F"
+  checksumType  = "sha256"
+  checksum64    = "F9C3147157DB32BEAB025314DB9B82C600E182D82994880C1D844C70E29D76EF"
+  checksumType64= "sha256"
 
-  silentArgs   = '/S'
+  silentArgs   = "/S"
   validExitCodes= @(0)
 }
 
 Install-ChocolateyPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-package
-Install-ChocolateyPath '$($env:SystemDrive)\Program Files\LLVM\bin' 'User' # Machine will assert administrative rights
+Install-ChocolateyPath "$($env:SystemDrive)\Program Files\LLVM\bin" "User" # Machine will assert administrative rights
